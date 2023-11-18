f0af9077c867219724e2bb6bc905cd4820737ede
Alex <Alexk.Thoo@gmail.com>
2023-11-17 16:14:05 -0800

    update models

--- app/models/spot_image.py
+++ app/models/spot_image.py
@@ -1,15 +1,26 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .spot import Spot
 
 class SpotImage(db.Model):
-    __tablename__ = 'spot_image'
+    __tablename__ = 'spot_images'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    spotId = db.Column(db.Integer, db.ForeignKey('spot.id'), primary_key=True)
     url = db.Column(db.String(255), nullable=False)
     preview = db.Column(db.Boolean)
 
-    spot = db.relationship('Spot', back_populates="spot_image")
+    #foreignkey
+    spotId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('spot.id')), primary_key=True)
+
+    #relationship
+    spot = db.relationship('Spot', back_populates="spot_images")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'spotId': self.spotId,
+            'url': self.url,
+            'preview': self.preview
+        }
