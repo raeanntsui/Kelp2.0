fb955d75729ba50943c1002ed27ffa4d5160e44a
Raeann <raeann.tsui@gmail.com>
2023-11-18 19:55:01 -0500

    adding app > api > spots_routes.py

---
+++ app/api/spots_routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import db, Spot
+
+
+spots_routes = Blueprint("spots", __name__)
+
+@spots_routes.route("/")
+def get_spots():
+    spots = Spot.query.all()
+    return jsonify([spot.to_dict() for spot in spots])
