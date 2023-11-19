c7566f3a22e4d21f70af3740ca59cd7d63495abe
Raeann <raeann.tsui@gmail.com>
2023-11-19 16:06:16 -0500

    create spot route draft

--- app/api/spots_routes.py
+++ app/api/spots_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-# from flask_login import login_required
+from flask_login import login_required
 from app.models import db, Spot
 from app.forms import SpotForm
 
@@ -7,30 +7,43 @@ spots_routes = Blueprint("spots", __name__)
 
 @spots_routes.route("/")
 def get_spots():
+    """
+    Get all spots
+    """
     spots = Spot.query.all()
     return jsonify([spot.to_dict() for spot in spots])
 
 @spots_routes.route("/<int:id>")
 def get_spot(id):
+    """
+    Get one spot
+    """
     spot = Spot.query.get(id)
     if spot:
         return spot.to_dict()
     else:
         return {"error": "Spot could not be found"}, 404
     
-# @spots_routes.route("/new", methods=["GET", "POST"])
-# def create_spot():
-#     # form = SpotForm()
-#     if request.method == "POST":
-#         # new_spot = Spot(
-#         business_name=request.form["business_name"],
-#         address=request.form["address"],
-#         city=request.form["city"],
-#         state=request.form["state"],
-#         zip_code=request.form["zip_code"],
-#         categories=request.form["categories"],
-#         open_hours=request.form["open_hours"],
-#         close_hours=request.form["close_hours"],
-#         description=request.form["description"],
-#         price_range=request.form["price_range"]
-#         # )
+@spots_routes.route("/new", methods=["POST"])
+@login_required
+def create_spot():
+    """
+    User must be logged in before creating a new spot
+    """
+    form = SpotForm()
+    if request.method == "POST":
+        new_spot = Spot(
+        business_name=request.form["business_name"],
+        address=request.form["address"],
+        city=request.form["city"],
+        state=request.form["state"],
+        zip_code=request.form["zip_code"],
+        categories=request.form["categories"],
+        open_hours=request.form["open_hours"],
+        close_hours=request.form["close_hours"],
+        description=request.form["description"],
+        price_range=request.form["price_range"]
+        )
+    db.session.add(new_spot)
+    db.session.commit()
+    return new_spot.to_dict()
