8b16fa54c7d23912fc5493dc2051747e8a34ce3d
Alex <Alexk.Thoo@gmail.com>
2023-11-19 11:43:29 -0800

    update review route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,14 +1,16 @@
 from flask import Blueprint, jsonify
 # from flask_login import login_required
-from app.models import db, Review
+from app.models import db, Review, Spot
 
 
 reviews_routes = Blueprint("reviews", __name__)
 
 @reviews_routes.route("/")
-def spot_reviews():
+def spot_reviews(spotId):
     '''
     get all reviews for a spot
     '''
+
+    product = Spot.querty.get(Spot)
     reviews = Review.query.all()
     return jsonify([review.to_dict() for review in reviews])
