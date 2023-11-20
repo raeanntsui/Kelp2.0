ae6eec233dcfc8084b7cceab13d47f2f714f95a8
Alex <Alexk.Thoo@gmail.com>
2023-11-19 17:47:24 -0800

    add deploy files

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -15,14 +15,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_spots()
-        undo_reviews()
         undo_spot_images()
+        undo_reviews()
+        undo_spots()
+        undo_users()
     seed_users()
     seed_spots()
     seed_reviews()
@@ -33,8 +33,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_spots()
-    undo_reviews()
     undo_spot_images()
+    undo_reviews()
+    undo_spots()
+    undo_users()
     # Add other undo functions here
