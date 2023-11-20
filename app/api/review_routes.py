dd8d6faaa6b33c124d3d039f599a476be8d8c463
Khuong Nguyen <thekhuong3991@gmail.com>
2023-11-19 17:57:24 -0800

    review_routes CRUD, working on create review

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 # from flask_login import login_required
 from app.models import db, Review
 
@@ -12,3 +12,36 @@ def spot_reviews():
     '''
     reviews = Review.query.all()
     return jsonify([review.to_dict() for review in reviews])
+
+@reviews_routes.route("/<int:spot_id>")
+def spot_reviews(spot_id):
+    '''
+    get all reviews for a specific spot
+    '''
+    reviews = Review.query.filter_by(spot_id=spot_id).all()
+    return jsonify([review.to_dict() for review in reviews])
+
+@reviews_routes.route("/<int:spot_id>", methods=["POST"])
+def create_review(spot_id):
+    '''
+    create a new review for a specific spot
+    '''
+    data = request.get_json()
+
+    description = data.get("description")
+    rating = data.get("rating")
+
+    if not all([description, rating]):
+        return jsonify({"error": "Review text and star rating are required"}), 400
+
+    if not 1 <= rating <= 5:
+        return jsonify({"error": "Star rating must be between 1 and 5"}), 400
+
+    new_review = Review(spot_id=spot_id, text=description, star_rating=rating)
+
+    db.session.add(new_review)
+    db.session.commit()
+
+    return jsonify({"message": "Review created successfully"})
+
+
