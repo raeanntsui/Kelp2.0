c40adf8abf2d1e0fc61d93eb104dc0eefceb3828
Min <minseok.km@gmail.com>
2023-11-22 11:06:11 -0500

    fuse search bar implemented

--- react-app/src/components/ShowAllSpots/ShowAllSpots.js
+++ react-app/src/components/ShowAllSpots/ShowAllSpots.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllSpotsThunk } from "../../store/spots";
+import Fuse from 'fuse.js';
 import "./ShowAllSpots.css";
 
 function ShowAllSpots() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredSpots, setFilteredSpots] = useState([]);
+
   const dispatch = useDispatch();
   const spots = useSelector((state) => state.spots.allSpots);
   // console.log("🚀🚀🚀🚀🚀🚀 ~ spots:", spots);
   const allSpots = Object.values(spots);
 
   const reviews = useSelector((state) => state.reviews.Reviews);
-  console.log("🚀🚀🚀🚀🚀🚀 ~ reviews:", reviews);
+  // console.log("🚀🚀🚀🚀🚀🚀 ~ reviews:", reviews);
+
+  const fuseOptions = {
+    keys: ['business_name', 'categories', 'price_range'],
+    includeScore: true,
+    threshold: 0.4,
+  };
+
+  const fuse = new Fuse(allSpots, fuseOptions);
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+
+    if (query.trim() === "") {
+      setFilteredSpots(allSpots);
+    } else {
+      const results = fuse.search(query);
+      setFilteredSpots(results.map((result) => result.item));
+    }
+  };
 
   useEffect(() => {
     dispatch(getAllSpotsThunk());
@@ -22,12 +45,17 @@ function ShowAllSpots() {
     return null;
   }
   //poo
-
   return (
     <>
       <h1>GET ALL SPOTS</h1>
+      <input
+        type="text"
+        placeholder="Search businesses..."
+        value={searchQuery}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
       <div className="spots-grid">
-        {allSpots.map((spot) => (
+        {filteredSpots.map((spot) => (
           <NavLink key={spot.id} to={`/spots/${spot.id}`}>
             {/* <img src={spot.spot_image} /> */}
             <h2>{spot.business_name}</h2>
@@ -45,5 +73,3 @@ function ShowAllSpots() {
 }
 
 export default ShowAllSpots;
-
-//push test2
