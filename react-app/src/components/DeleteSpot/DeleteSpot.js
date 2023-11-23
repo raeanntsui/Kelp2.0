296decff1904edba75e3ed03027fe2f97ccca317
Alex <Alexk.Thoo@gmail.com>
2023-11-23 01:56:49 -0800

    spot delete progress

--- react-app/src/components/DeleteSpot/DeleteSpot.js
+++ react-app/src/components/DeleteSpot/DeleteSpot.js
@@ -3,43 +3,37 @@ import { useDispatch } from "react-redux";
 import { deleteSpotThunk } from "../../store/spots";
 import "./DeleteSpot.css";
 import { useModal } from "../../context/Modal";
+import { useHistory } from "react-router-dom";
 
-function DeleteSpot({ spot }) {
+function DeleteSpot({ spotId }) {
   const { closeModal } = useModal();
   const dispatch = useDispatch();
-  const [exists, setExists] = useState(true);
+  const [errors, setErrors] = useState({});
+  const { push } = useHistory();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(deleteSpotThunk(spot.id));
-    setExists(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await dispatch(deleteSpotThunk(spotId));
     closeModal();
   };
-
-  const doNotDeleteAndCloseModal = (event) => {
-    event.preventDefault();
+  const handleCancel = async (e) => {
+    e.preventDefault();
     closeModal();
   };
-
   return (
     <>
-      {exists && (
-        <div id="delete-review">
-          <h1>Confirm Delete</h1>
-          <h2>Are you sure you want to remove this spot from the listings?</h2>
-          <div id="delete-review-button">
-            <button id="top-button-delete" onClick={handleSubmit}>
-              Yes (Delete Spot)
-            </button>
-            <button
-              id="bottom-button-delete"
-              onClick={doNotDeleteAndCloseModal}
-            >
-              No (Keep Spot)
-            </button>
-          </div>
+      <div id="delete-review">
+        <h1>Confirm Delete</h1>
+        <h2>Are you sure you want to remove this spot from the listings?</h2>
+        <div id="delete-review-button">
+          <button id="top-button-delete" onClick={handleSubmit}>
+            Yes (Delete Spot)
+          </button>
+          <button id="bottom-button-delete" onClick={handleCancel}>
+            No (Keep Spot)
+          </button>
         </div>
-      )}
+      </div>
     </>
   );
 }
