b14f719a8a40b3b5992d5a2d867800462dcf11b0
Raeann <raeann.tsui@gmail.com>
2023-11-24 01:27:19 -0500

    css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,28 +8,58 @@ function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink exact to="/spots">
-          Spots
-        </NavLink>
-      </li>
-      <li>
-        <NavLink exact to="/spots/new">
-          Create New Spot
-        </NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-    </ul>
+    <>
+      <div id="upper-half-homepage">
+        <div id="top-nav">
+          <div id="kelp-logo">
+            <NavLink exact to="/">
+              Kelp
+            </NavLink>{" "}
+            <i class="fa-brands fa-yelp"></i>
+          </div>
+          <div id="searchbar">
+            <input
+              type="text"
+              placeholder="things to do, nail salons, plumbers"
+            />
+            <h1>|</h1>
+            <input type="text" placeholder="San Francisco, CA" />
+            <button id="search-icon" type="submit" className="search-button">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+          <div id="homepage-buttons">
+            <button id="write-review-button">Write a Review</button>
+            <button id="login-button">Log In</button>
+            <button id="signup-button">Sign Up</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/spots">
+              Spots
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/spots/new">
+              Create New Spot
+            </NavLink>
+          </li>
+          {isLoaded && (
+            <li>
+              <ProfileButton user={sessionUser} />
+            </li>
+          )}
+        </ul>
+      </div>
+    </>
   );
 }
 
