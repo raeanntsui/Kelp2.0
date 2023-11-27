31ac596ebe858117b39fac48ea9408ab47013e67
Min <minseok.km@gmail.com>
2023-11-27 02:55:39 -0500

    feature for results not found

--- react-app/src/components/ShowAllSpots/ShowAllSpots.js
+++ react-app/src/components/ShowAllSpots/ShowAllSpots.js
@@ -40,7 +40,6 @@ function ShowAllSpots() {
   let count = 1;
   return (
     <>
-      {/* <h1>GET ALL SPOTS</h1> */}
       <input
         type="text"
         value={searchInput}
@@ -48,50 +47,59 @@ function ShowAllSpots() {
         placeholder="Search by name, category, or price range"
       />
       <div className="spots-front-page">
-
-        <div className="spots-grid">
-          {filteredSpots.map((spot) => (
-            <NavLink
-              key={spot.id}
-              to={`/spots/${spot.id}`}
-              id="spots-grid-navlink">
-              <div className="spots-grid-each">
-                <div className="each-spot">
-                  <div className="each-spots-image">
-                    {/* <img src={spot.spot_image} /> */}
-                    <img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/21/20/campaign_images/fbf76a44e63d/could-you-pass-an-interview-and-get-hired-at-the--2-2131-1574368600-0_dblbig.jpg?resize=1200:*" />
-                  </div>
-                  <div className="each-spots-info">
-                    <h2 id="spot-title">
-                      {count++}. {spot.business_name}
-                    </h2>
-                    <div className="filled-star">
-                      <p>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-solid fa-star"></i>
-                        <i className="fa-regular fa-star"></i>
+        {filteredSpots.length === 0 ? (
+          <div className="result-not-found">No results found</div>
+        ) : (
+          <div className="spots-grid">
+            {filteredSpots.map((spot) => (
+              <NavLink
+                key={spot.id}
+                to={`/spots/${spot.id}`}
+                id="spots-grid-navlink"
+              >
+                <div className="spots-grid-each">
+                  <div className="each-spot">
+                    <div className="each-spots-image">
+                      <img
+                        src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/21/20/campaign_images/fbf76a44e63d/could-you-pass-an-interview-and-get-hired-at-the--2-2131-1574368600-0_dblbig.jpg?resize=1200:*"
+                        alt="Spot Image"
+                      />
+                    </div>
+                    <div className="each-spots-info">
+                      <h2 id="spot-title">
+                        {count++}. {spot.business_name}
+                      </h2>
+                      <div className="filled-star">
+                        <p>
+                          <i className="fa-solid fa-star"></i>
+                          <i className="fa-solid fa-star"></i>
+                          <i className="fa-solid fa-star"></i>
+                          <i className="fa-solid fa-star"></i>
+                          <i className="fa-regular fa-star"></i>
+                        </p>
+                      </div>
+                      <span className="categories">{spot.categories}</span>
+                      <div id="open-time">
+                        <p className="opening-hours">
+                          <span className="open">Open</span> until{" "}
+                          {spot.close_hours - 12}:00 PM
+                        </p>
+                      </div>
+                      <p className="spot-description ellipsis-text">
+                        {spot.description}
                       </p>
                     </div>
-                    <span className="categories">{spot.categories}</span>
-                    <div id="open-time">
-                      <p className="opening-hours">
-                        <span className="open">Open</span> until{" "}
-                        {spot.close_hours - 12}:00 PM
-                      </p>
-                    </div>
-                    <p className="spot-description ellipsis-text">
-                      {spot.description}
-                    </p>
                   </div>
                 </div>
-              </div>
-            </NavLink>
-          ))}
-        </div>
+              </NavLink>
+            ))}
+          </div>
+        )}
         <div className="google-maps-grid">
-          <img src="https://www.google.com/maps/d/thumbnail?mid=149XtkqRiBZ68Y28N8Px4kMh9ld4&hl=en" />
+          <img
+            src="https://www.google.com/maps/d/thumbnail?mid=149XtkqRiBZ68Y28N8Px4kMh9ld4&hl=en"
+            alt="Google Maps Thumbnail"
+          />
         </div>
       </div>
     </>
