2cc898e9e1a1848736a233b379164d2a2024d454
Raeann <raeann.tsui@gmail.com>
2023-11-28 05:03:36 -0500

    hide review

--- react-app/src/components/DeleteReview/index.js
+++ react-app/src/components/DeleteReview/index.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import { useModal } from "../../context/Modal";
 import { useDispatch } from "react-redux";
 import { deleteReviewThunk } from "../../store/reviews";
-
+import "./DeleteReview.css";
 export default function DeleteReview({ review }) {
   const { closeModal } = useModal();
   const dispatch = useDispatch();
   const [deleted, setDeleted] = useState(true);
-
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -28,8 +26,12 @@ export default function DeleteReview({ review }) {
         <div>
           <h1>Confirm Delete</h1>
           <h2>Are you sure you want to delete this review?</h2>
-          <button onClick={handleSubmit}>Yes (Delete Review)</button>
-          <button onClick={closeTheModal}>No (Keep Review)</button>
+          <button className="delete-button" onClick={handleSubmit}>
+            Yes (Delete Review)
+          </button>
+          <button className="delete-button" onClick={closeTheModal}>
+            No (Keep Review)
+          </button>
         </div>
       )}
     </>
